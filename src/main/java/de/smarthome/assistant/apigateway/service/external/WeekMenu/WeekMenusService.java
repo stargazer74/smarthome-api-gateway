13c6006413ce0ee011b7f99ef22af09196b9c71b
Chris <chris@antisphex.net>
2019-10-11 22:15:25 +0200

    WIP

--- src/main/java/de/smarthome/assistant/apigateway/service/external/WeekMenu/WeekMenusService.java
+++ src/main/java/de/smarthome/assistant/apigateway/service/external/WeekMenu/WeekMenusService.java
@@ -57,62 +57,15 @@ public class WeekMenusService {
     public CompletableFuture<Optional<WeekMenuListResponseDto>> getWeekMenuList() {
         try {
             log.info("Looking up weekmenu service");
-            final UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(serviceUrl).path("/list").port(servicePort)
+            final UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host(serviceUrl).path("/menu-service/list").port(servicePort)
                     .build();
             final RestTemplate restTemplate = new RestTemplate();
             final String uriString = uriComponents.toUriString();
             final WeekMenuListResponseDto weekMenuListResponseDto = restTemplate.getForObject(uriString, WeekMenuListResponseDto.class);
             return CompletableFuture.completedFuture(Optional.ofNullable(weekMenuListResponseDto));
         } catch (RestClientException e) {
-            log.info(e.getMessage());
+            log.info("Can't connect to weekmenu service due the following error: " + e.getMessage());
             return CompletableFuture.completedFuture(Optional.empty());
         }
     }
-
-    private WeekMenuListResponseDto mockData() {
-        // create demo data
-        final WeekMenuResponseDto weekMenuResponseDto_1 = new WeekMenuResponseDto();
-        weekMenuResponseDto_1.setId(1L);
-        weekMenuResponseDto_1.setName("Schweinefleisch mit Klößen");
-        final IngredientResponseDto ingredientResponse_1 = new IngredientResponseDto();
-        ingredientResponse_1.setId(1L);
-        ingredientResponse_1.setName("Kartoffeln");
-        ingredientResponse_1.setAmount("200");
-        ingredientResponse_1.setUnitOfMeasure(UnitOfMeasures.KILOGRAMM);
-        final IngredientResponseDto ingredientResponse_2 = new IngredientResponseDto();
-        ingredientResponse_2.setId(2L);
-        ingredientResponse_2.setName("Rinderfleisch");
-        ingredientResponse_2.setAmount("500");
-        ingredientResponse_2.setUnitOfMeasure(UnitOfMeasures.LITER);
-        List<IngredientResponseDto> ingredients_1 = new ArrayList<>();
-        ingredients_1.add(ingredientResponse_1);
-        ingredients_1.add(ingredientResponse_2);
-        weekMenuResponseDto_1.setIngredients(ingredients_1);
-
-        final WeekMenuResponseDto weekMenuResponseDto_2 = new WeekMenuResponseDto();
-        weekMenuResponseDto_2.setId(2L);
-        weekMenuResponseDto_2.setName("Grießbrei");
-        final IngredientResponseDto ingredientResponse_3 = new IngredientResponseDto();
-        ingredientResponse_3.setId(3L);
-        ingredientResponse_3.setName("Grieß");
-        ingredientResponse_3.setAmount("200");
-        ingredientResponse_3.setUnitOfMeasure(UnitOfMeasures.LITER);
-        final IngredientResponseDto ingredientResponse_4 = new IngredientResponseDto();
-        ingredientResponse_4.setId(4L);
-        ingredientResponse_4.setName("Zucker");
-        ingredientResponse_4.setAmount("2");
-        ingredientResponse_4.setUnitOfMeasure(UnitOfMeasures.KILOGRAMM);
-        List<IngredientResponseDto> ingredients_2 = new ArrayList<>();
-        ingredients_2.add(ingredientResponse_3);
-        ingredients_2.add(ingredientResponse_4);
-        weekMenuResponseDto_2.setIngredients(ingredients_2);
-
-        List<WeekMenuResponseDto> weekMenuResponseDtos = new ArrayList<>();
-        weekMenuResponseDtos.add(weekMenuResponseDto_1);
-        weekMenuResponseDtos.add(weekMenuResponseDto_2);
-        WeekMenuListResponseDto weekMenuListResponseDto = new WeekMenuListResponseDto();
-        weekMenuListResponseDto.setWeekMenuResponseDtos(weekMenuResponseDtos);
-        return weekMenuListResponseDto;
-    }
-
 }
